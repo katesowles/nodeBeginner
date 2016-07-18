958fa257a6b212734b4f5871022bdb45224e9642
Kate Sowles <katesowles@gmail.com>
2016-07-18 10:39:32 -0700

    progress before class monday

--- httpserver/requesthandlers.js
+++ httpserver/requesthandlers.js
@@ -1,12 +1,38 @@
-function start () {
-  console.log(`request handler 'start' was called.`);
+const exec = require('child_process').exec;
+const querystring = require('querystring');
+const fs = require('fs');
 
-  // let body = '<html>' + '<head>' + '<meta http-equiv="Content-Type" content = "text/html; charset=UTF-8" />' + '</head>' + '<body>' +
+function start (response, postData) {
+  console.log('request handler \'start\' was called.');
+
+  let body = '<html>' +
+      '<head>' +
+        '<meta http-equiv="Content-Type" content="text/html charset=UTF-8" />' +
+      '</head>' +
+      '<body>' +
+        '<form action="/upload" method="post">' +
+          '<textarea name="text" rows="20" cols="60"></textarea>' +
+          '<input type="submit" value="submit text" />' +
+          '</form>' +
+      '</body>' +
+    '</html>';
+
+  exec('ls -lah', function (error, stdout, stderr) {
+    response.writeHead(200, {'Content-Type' : 'text/html'});
+    response.write(body);
+    response.end();
+  });
 }
 
-function upload() {
-  console.log(`request handler 'upload' was called.`);
+function upload (response, postData) {
+  console.log('request handler \'upload\' was called.');
+  response.writeHead(200, {'Content-Type' : 'text/plain'});
+  response.write('you\'ve sent the following text: ' +
+  querystring.parse(postData).text);
+  response.end();
 }
+
+
 
 exports.start = start;
 exports.upload = upload;
--- httpserver/server.js
+++ httpserver/server.js
@@ -3,34 +3,29 @@
 
 const http = require('http');
 const url = require('url');
-const router = require('./router');
-const requestHandlers = require('./requesthandlers');
+// const route = require('./router').route;
+// const requestHandlers = require('./requesthandlers');
 
-function start (handle) {
+function start (route, handle) {
   http.createServer(function(request, response) {
     // allows us to distinguish requests based on the URL path requested
-    var pathname = url.parse(request.url).pathname;
+    let postData = '';
+    let pathname = url.parse(request.url).pathname;
     console.log(`request for ${pathname} received.`);
 
-    router.route(handle, pathname);
+    request.setEncoding('utf8');
 
-    response.writeHead(200, {"Content-Type" : "text/plain"});
-    response.write('hello world');
-    response.end();
+    request.addListener('data', function (postDataChunk) {
+      postData += postDataChunk;
+      console.log('recieved POST data chunk "' + postDataChunk + '"');
+    });
+
+    request.addListener('end', function() {
+      route(handle, pathname, response, postData);
+    });
+
   }).listen(8888);
   console.log('server has started');
 }
 
 exports.start = start;
-
-
-// works the same as :
-//
-// const http = require('http');
-//
-// function onRequest(request, response) {
-//   response.writeHead(200, {"Content-Type" : "text/plain"});  // this tells the browser that the HTTP status is 200 (ok) and that the content coming through is text
-//   response.write('hello world');  // writes this text to the body of the page upon load
-//   response.end();  // once that's done, it ends the connection
-// }
-// http.createServer(onRequest).listen(8888);
